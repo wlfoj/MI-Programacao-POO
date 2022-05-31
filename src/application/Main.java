29145d07b3f4d8932aed2d2f2d571d15a8132f48
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2022-05-31 20:06:46 -0300

    javaFXicons

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
@@ -14,6 +15,10 @@ public class Main extends Application {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Image anotherIcon = new Image("file:///C:/Users/tassi/eclipse-workspace/comercialV2/src/iconapp.png");
+			primaryStage.getIcons().add(anotherIcon);
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
