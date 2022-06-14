b752b6ce8c52247e07736e04c4992ad827f79809
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2022-06-14 15:47:14 -0300

    add Itens

---
+++ src/controllers/FormProviderController.java
@@ -0,0 +1,65 @@
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import main.Main;
+import model.Product;
+
+public class FormProviderController implements Initializable {
+
+    @FXML
+    private Button btnAddProduct,btnBack,btnDeleteProduct,btnSave;
+
+    @FXML
+    private ComboBox<Product> comboProducts;
+
+    @FXML
+    private TextField inputAdress, inputName, inputCnpj;
+    
+    @FXML
+    private TableView<?> tableProducts;
+
+    @FXML
+    void actionAddProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionBack(ActionEvent event) throws IOException {
+    	AnchorPane anchor = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/GerenciadorProvider.fxml"));
+		Scene cena = new Scene(anchor);
+		Main.setScene(cena);
+    }
+
+    @FXML
+    void actionDeleteProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickLine(MouseEvent event) {
+
+    }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Product p = new Product();
+		p.setName("pao");
+		p.setId(0);
+		//comboProducts.setAccessibleText("olá");
+		comboProducts.setValue(p);
+	}
+}
--- src/controllers/ProviderController.java
+++ src/controllers/ProviderController.java
@@ -1,34 +1,58 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import main.Main;
 
 public class ProviderController implements Initializable{
 
 	@FXML
-	private void eventoOne(ActionEvent e) {
-		System.out.println("Voltar Fornecedor para Menu");
-		Main.scenes("backfornecedor");
+	private void actionBack(ActionEvent e) throws IOException {
+		AnchorPane anchor = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/Menu.fxml"));
+		Scene cena = new Scene(anchor);
+		Main.setScene(cena);
 	}
 	
+    @FXML
+    void actionDelete(ActionEvent event) {
+
+    }
+	
+    @FXML
+    void actionEdit(ActionEvent event) {
+
+    }
+	
+    @FXML
+    void actionCreate(ActionEvent event) throws IOException {
+    	Main.setIdSelected(-1);
+		AnchorPane anchor = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/FormularioProvider.fxml"));
+		Scene cena = new Scene(anchor);
+		Main.setScene(cena);
+    }
+	
 	@FXML
-    private Button btt1, btt2, btt3, btt4, btt5;
+    private Button btnBack, btnCreate, btnEdit, btnDelete; //btt3, btt4, //btt5;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		btt1.setCursor(Cursor.HAND);
-		btt2.setCursor(Cursor.HAND);
-		btt3.setCursor(Cursor.HAND);
-		btt4.setCursor(Cursor.HAND);
-		btt5.setCursor(Cursor.HAND);
-		//criar um for checkbox
+		btnBack.setCursor(Cursor.HAND);
+		btnCreate.setCursor(Cursor.HAND);
+		btnDelete.setCursor(Cursor.HAND);
+		btnEdit.setCursor(Cursor.HAND);
+
+		
+		
 	}
 
 	
