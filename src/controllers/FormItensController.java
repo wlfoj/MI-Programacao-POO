25934ec27dda791428dc9b4d877cecfb674c59dc
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2022-07-02 16:18:33 -0300

    edit

--- src/controllers/FormItensController.java
+++ src/controllers/FormItensController.java
@@ -1,30 +1,42 @@
 package controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import main.Main;
+import model.ManagementProducts;
+import model.Product;
 
-public class FormItensController {
+public class FormItensController implements Initializable {
+	ObservableList<Product> observableListaProduct; 
 
-    @FXML
+	@FXML
     void eventBack(ActionEvent event) throws IOException {
     	AnchorPane anchor = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/GerenciadorItens.fxml"));
 		Scene cena = new Scene(anchor);
 		Main.setScene(cena);
     }
 
-
+    
     @FXML
     private Button btnAdicionar, btnSave, btnBack, btnDelete;
 
@@ -40,6 +52,18 @@ public class FormItensController {
     @FXML
     private Label labelUnd;
 
+    @FXML
+    private TableView<Product> tableProductes;
+    
+    @FXML
+    private TableColumn<Product, String> tableMedida;
+    
+    @FXML
+    private TableColumn<Product, String> tableProduct;
+
+    @FXML
+    private TableColumn<Product, Integer> tableQtd;
+    
     @FXML
     void clickLine(MouseEvent event) {
     	btnDelete.setDisable(false);
@@ -64,6 +88,21 @@ public class FormItensController {
     void clickComboProduct(MouseEvent event) {
     	inputQuatity.setDisable(false);
     }
+    
+    public void refreshTableViewProducts() {
+		observableListaProduct = FXCollections.observableArrayList(ManagementProducts.listAllProducts());
+		tableProductes.setItems(observableListaProduct);
+		
+		tableMedida.setCellValueFactory(new PropertyValueFactory<>("medida"));
+		tableProduct.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tableQtd.setCellValueFactory(new PropertyValueFactory<>("qtd"));
+    }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		refreshTableViewProducts();
+		
+	}
 
 }
 
